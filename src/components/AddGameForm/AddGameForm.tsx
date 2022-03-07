2ee25dabe31e8f9e15287c4239105c8a304c9040
krismackdev <krismackdev@gmail.com>
2022-03-07 03:23:00 -0500

    finished reducer for add new game input

--- src/components/AddGameForm/AddGameForm.tsx
+++ src/components/AddGameForm/AddGameForm.tsx
@@ -5,6 +5,8 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  MenuItem,
+  Select,
   TextField,
 } from '@mui/material'
 // @ts-ignore
@@ -20,13 +22,13 @@ interface GameObject {
   id: string
   image: string
   link: string
-  // players: {
-  //   one: PlayerSuitability
-  //   two: PlayerSuitability
-  //   three: PlayerSuitability
-  //   four: PlayerSuitability
-  //   five: PlayerSuitability
-  // }
+  players: {
+    one: PlayerSuitability
+    two: PlayerSuitability
+    three: PlayerSuitability
+    four: PlayerSuitability
+    five: PlayerSuitability
+  }
   title: string
 }
 
@@ -34,13 +36,13 @@ interface ActionObject {
   type:
     | 'changeImage'
     | 'changeLink'
-    // | 'changePlayerOne'
-    // | 'changePlayerTwo'
-    // | 'changePlayerThree'
-    // | 'changePlayerFour'
-    // | 'changePlayerFive'
+    | 'changePlayerOne'
+    | 'changePlayerTwo'
+    | 'changePlayerThree'
+    | 'changePlayerFour'
+    | 'changePlayerFive'
     | 'changeTitle'
-  payload?: string
+  payload: string
 }
 
 const addGameStateReducer = (state: GameObject, action: ActionObject) => {
@@ -51,6 +53,81 @@ const addGameStateReducer = (state: GameObject, action: ActionObject) => {
       return { ...state, image: action.payload! }
     case 'changeLink':
       return { ...state, link: action.payload! }
+    case 'changePlayerOne':
+      if (
+        action.payload === 'no' ||
+        action.payload === 'ok' ||
+        action.payload === 'yes'
+      ) {
+        return {
+          ...state,
+          players: {
+            ...state.players,
+            one: action.payload as PlayerSuitability,
+          },
+        }
+      }
+      return state
+    case 'changePlayerTwo':
+      if (
+        action.payload === 'no' ||
+        action.payload === 'ok' ||
+        action.payload === 'yes'
+      ) {
+        return {
+          ...state,
+          players: {
+            ...state.players,
+            two: action.payload as PlayerSuitability,
+          },
+        }
+      }
+      return state
+    case 'changePlayerThree':
+      if (
+        action.payload === 'no' ||
+        action.payload === 'ok' ||
+        action.payload === 'yes'
+      ) {
+        return {
+          ...state,
+          players: {
+            ...state.players,
+            three: action.payload as PlayerSuitability,
+          },
+        }
+      }
+      return state
+    case 'changePlayerFour':
+      if (
+        action.payload === 'no' ||
+        action.payload === 'ok' ||
+        action.payload === 'yes'
+      ) {
+        return {
+          ...state,
+          players: {
+            ...state.players,
+            four: action.payload as PlayerSuitability,
+          },
+        }
+      }
+      return state
+    case 'changePlayerFive':
+      if (
+        action.payload === 'no' ||
+        action.payload === 'ok' ||
+        action.payload === 'yes'
+      ) {
+        return {
+          ...state,
+          players: {
+            ...state.players,
+            five: action.payload as PlayerSuitability,
+          },
+        }
+      }
+      return state
   }
 }
 
@@ -60,13 +137,13 @@ const AddGameForm: React.FC<AddGameFormProps> = ({ setFormIsActive }) => {
     image: '',
     link: '',
     title: '',
-    // players: {
-    //   one: 'no',
-    //   two: 'no',
-    //   three: 'no',
-    //   four: 'no',
-    //   five: 'no',
-    // },
+    players: {
+      one: 'no',
+      two: 'no',
+      three: 'no',
+      four: 'no',
+      five: 'no',
+    },
   }
 
   const [addGameState, dispatch] = useReducer(
@@ -76,9 +153,7 @@ const AddGameForm: React.FC<AddGameFormProps> = ({ setFormIsActive }) => {
 
   return (
     <Dialog open={true} onClose={() => setFormIsActive(false)}>
-      <DialogTitle sx={{ textAlign: 'center' }}>
-        <h4>Add a new game</h4>
-      </DialogTitle>
+      <DialogTitle sx={{ textAlign: 'center' }}>Add a new game</DialogTitle>
       <DialogContent>
         <TextField
           label="Title"
@@ -104,6 +179,62 @@ const AddGameForm: React.FC<AddGameFormProps> = ({ setFormIsActive }) => {
             dispatch({ type: 'changeLink', payload: e.target.value })
           }
         />
+        <Select
+          defaultValue="no"
+          size="small"
+          onChange={e =>
+            dispatch({ type: 'changePlayerOne', payload: e.target.value })
+          }
+        >
+          <MenuItem value="no">no</MenuItem>
+          <MenuItem value="ok">ok</MenuItem>
+          <MenuItem value="yes">yes</MenuItem>
+        </Select>
+        <Select
+          defaultValue="no"
+          size="small"
+          onChange={e =>
+            dispatch({ type: 'changePlayerTwo', payload: e.target.value })
+          }
+        >
+          <MenuItem value="no">no</MenuItem>
+          <MenuItem value="ok">ok</MenuItem>
+          <MenuItem value="yes">yes</MenuItem>
+        </Select>
+        <Select
+          defaultValue="no"
+          size="small"
+          onChange={e =>
+            dispatch({ type: 'changePlayerThree', payload: e.target.value })
+          }
+        >
+          <MenuItem value="no">no</MenuItem>
+          <MenuItem value="ok">ok</MenuItem>
+          <MenuItem value="yes">yes</MenuItem>
+        </Select>
+        <Select
+          defaultValue="no"
+          size="small"
+          onChange={e =>
+            dispatch({ type: 'changePlayerFour', payload: e.target.value })
+          }
+        >
+          <MenuItem value="no">no</MenuItem>
+          <MenuItem value="ok">ok</MenuItem>
+          <MenuItem value="yes">yes</MenuItem>
+        </Select>
+        <Select
+          defaultValue="no"
+          size="small"
+          onChange={e =>
+            dispatch({ type: 'changePlayerFive', payload: e.target.value })
+          }
+        >
+          {' '}
+          <MenuItem value="no">no</MenuItem>
+          <MenuItem value="ok">ok</MenuItem>
+          <MenuItem value="yes">yes</MenuItem>
+        </Select>
         <DialogActions>{/* ADD & CLOSE BUTTON HERE */}</DialogActions>
       </DialogContent>
     </Dialog>
