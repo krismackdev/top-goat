2625e5555cf35ff2390e31102c374d89c22ed037
krismackdev <krismackdev@gmail.com>
2022-03-14 01:23:00 -0400

    finished all cases in cloud function syncMatchArray

--- functions/index.js
+++ functions/index.js
@@ -11,15 +11,41 @@ exports.syncMatchArray = functions.firestore
       const {before, after} = change;
       const matchId = context.params.matchId;
       let forGameId;
-      functions.logger.log("I AM RUNNING BEFORE IF!");
-      // for a new match...
+      functions.logger.log("syncMatchArray RUNNING...");
+
+      // handle a new match...
       if (!before.exists) {
         forGameId = after.data().gameId;
-        functions.logger.log("I AM RUNNING!");
+        functions.logger.log("handling a new match...");
         db.collection("games").doc(`${forGameId}`).update({
           matches: admin.firestore.FieldValue.arrayUnion(matchId),
         });
       }
-      // TO ADD: if deleted, if updated...
+
+      // handle a deleted match...
+      if (!after.exists) {
+        forGameId = before.data().gameId;
+        functions.logger.log("handling a deleted match...");
+        db.collection("games").doc(`${forGameId}`).update({
+          matches: admin.firestore.FieldValue.arrayRemove(matchId),
+        });
+      }
+
+      // handle an updated match...
+      if (before.exists && after.exists) {
+        functions.logger.log("handling an updated match...");
+        if (before.data().gameId !== after.data().gameId) {
+          functions.logger.log("updated match has new gameId...");
+          const oldGameId = before.data().gameId;
+          const newGameId = after.data().gameId;
+          db.collection("games").doc(`${oldGameId}`).update({
+            matches: admin.firestore.FieldValue.arrayRemove(matchId),
+          });
+          db.collection("games").doc(`${newGameId}`).update({
+            matches: admin.firestore.FieldValue.arrayUnion(matchId),
+          });
+        }
+      }
+
       return null;
     });
